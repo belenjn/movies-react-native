3390d4339feb69b049017b78b4f193cacff73359
Belen JN <belenjarabanavas31@gmail.com>
2022-12-13 15:06:31 +0100

    Testing routes

--- src/navigaton/Navigation.tsx
+++ src/navigaton/Navigation.tsx
@@ -7,7 +7,13 @@ const Stack = createStackNavigator();
 
 export const Navigation = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: 'white',
+        },
+      }}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Details" component={DetailScreen} />
     </Stack.Navigator>
