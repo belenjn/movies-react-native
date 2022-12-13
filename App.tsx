391824ecc3db8b5f719282c0146d34627e85be34
Belen JN <belenjarabanavas31@gmail.com>
2022-12-13 14:49:37 +0100

    React-navigation-stack installed

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {Text, View} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
   );
 };
 
