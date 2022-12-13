fc876c676e4b8f73c80fe8845213024aa7cdd910
Belen JN <belenjarabanavas31@gmail.com>
2022-12-13 14:55:06 +0100

    Navigation component created and some Screens too

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {Navigation} from './src/navigaton/Navigation';
 
 const App = () => {
   return (
     <NavigationContainer>
-
+      <Navigation />
     </NavigationContainer>
   );
 };
---
+++ src/navigaton/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen} from '../screens/HomeScreen';
+import {DetailScreen} from '../screens/DetailScreen';
+
+const Stack = createStackNavigator();
+
+export const Navigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailScreen} />
+    </Stack.Navigator>
+  );
+};
