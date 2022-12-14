c431696dbfeb87450b6527126f7011364c9fd2dd
Belen JN <belenjarabanavas31@gmail.com>
2022-12-14 11:27:35 +0100

    New changes in interface

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {useState, useEffect} from 'react';
 import movieDB from '../api/movieDB';
-import {Movie, MovieDBNowPlaying} from '../interfaces/movieInterface';
+import {Movie, MovieDBNowResponse} from '../interfaces/movieInterface';
 
 export const useMovies = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [moviesInCinema, setMoviesInCinema] = useState<Movie[]>([]);
 
   const getMovies = async () => {
-    const resp = await movieDB.get<MovieDBNowPlaying>('/now_playing');
+    const resp = await movieDB.get<MovieDBNowResponse>('/now_playing');
     const movies = resp.data.results;
     setMoviesInCinema(movies);
 
