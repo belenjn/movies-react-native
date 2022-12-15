9ddd98220e10f967bfc7921e6f22859640e4d187
Belen JN <belenjarabanavas31@gmail.com>
2022-12-15 10:35:47 +0100

    Styling GradientBackground

--- src/components/GradientBackground.tsx
+++ src/components/GradientBackground.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 interface Props {
   children: JSX.Element | JSX.Element[];
 }
 
 export const GradientBackground = ({children}: Props) => {
-  return <View style={{flex: 1, backgroundColor: 'blue'}}>{children}</View>;
+  return (
+    <View style={{flex: 1}}>
+      <LinearGradient
+        colors={['#084f6a', '#75cedb', 'white']}
+        style={{...StyleSheet.absoluteFillObject}}
+      />
+    </View>
+  );
 };
