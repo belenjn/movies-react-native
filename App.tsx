c1b71d1723464118e97ea3361017942dbce2c12a
Belen JN <belenjarabanavas31@gmail.com>
2022-12-15 10:17:35 +0100

    Example of fadeIn and fadeOut

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {Navigation} from './src/navigaton/Navigation';
+// import {Navigation} from './src/navigaton/Navigation';
 import {StatusBar} from 'react-native';
+import {FadeScreen} from './src/screens/FadeScreen';
 
 const App = () => {
   return (
     <NavigationContainer>
       <StatusBar backgroundColor="black" barStyle="light-content" />
-      <Navigation />
+      {/* <Navigation /> */}
+      <FadeScreen />
     </NavigationContainer>
   );
 };
